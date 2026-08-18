4b1ecd1abb3f7b6c7348a1ff250efae7e3e7464d
Shijin P <shijinp006@gmail.com>
2026-08-18 16:03:32 +0530

    first commit

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,25 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Tables from './pages/Admin/tables&qrcode/Tables&QRCode';
-import './App.css';
 
 function App() {
-  const [activeTableOrder, setActiveTableOrder] = useState(null);
-
-
-
   return (
-    <div className="w-full min-h-screen bg-[#0B0E14]">
-      {activeTableOrder ? (
-        <FoodOrderForm
-          tableNo={activeTableOrder}
-          onBackToAdmin={handleBackToAdmin}
-        />
-      ) : (
-        <>
-          <Dashboard onOpenOrderForm={handleOpenOrderForm} />
-        </>
-      )}
-    </div>
+    <>
+      <Tables />
+    </>
   );
 }
 
